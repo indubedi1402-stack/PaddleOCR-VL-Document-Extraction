9ff7fef1c68ad25b7fd3525e9bade7de5226553c
Indu Bedi <indubedi1402@gmail.com>
2026-09-17 19:30:31 +0530

    feat: add shared field-pattern module

---
+++ paddleocr_vl_document_extraction/patterns.py
@@ -0,0 +1,7 @@
+PATTERNS = {
+    "invoice_number": r"Invoice Number:\s*(.+)",
+    "date": r"Date:\s*(.+)",
+    "vendor": r"Vendor:\s*(.+)",
+    "customer": r"Customer:\s*(.+)",
+    "total_amount": r"Total Amount:\s*\$?\s*([\d,.]+)",
+}
